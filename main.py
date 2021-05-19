f5972931ddbf48b7e2f48f9f8b36c25aa0e9ac80
cianbrophy <cianbrophy7@gmail.com>
2021-05-19 13:29:57 +0100

    user input for web scraper functional

--- main.py
+++ main.py
@@ -1,32 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://ie.indeed.com/jobs?q=&l=Ireland'
+## function to find jobs
+def findJobs(URL):
+    page = requests.get(URL)
 
-page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-soup = BeautifulSoup(page.content, 'html.parser')
-
-job_elems = soup.find_all('div', 'jobsearch-SerpJobCard')
+    job_elems = soup.find_all('div', 'jobsearch-SerpJobCard')
 
 
-for job_elem in job_elems:
+    for job_elem in job_elems:
     
-    title = job_elem.find('h2', class_ = 'title')
+        title = job_elem.find('h2', class_ = 'title')
 
-    company = job_elem.find('span', class_ = 'company')
-    if None in company:
-        continue
+        company = job_elem.find('span', class_ = 'company')
+        if company is None:
+                continue
 
-    location = job_elem.find('span',class_ = 'location')
-    if None in location:
-        continue
+        location = job_elem.find('span',class_ = 'location')
+        if location is None:
+            continue
 
-    if "new" in title.text:
-        title.text.replace("new", "")
 
-    print(title.text.strip())
-    print(location.text.strip())
-    print(company.text.strip())
-    print()
+        print("\nJob Title: " + title.text.strip())
+        print("Location: " + location.text.strip())
+        print("Company Name: " + company.text.strip())
+        print()
 
+keyword = input("Enter any keywords (jobs, title, company etc.):\n")
+location = input("Enter your preferred location:\n")
+
+URL = 'https://ie.indeed.com/jobs?q=' + keyword + '&l=' + location + ''
+
+print(URL)
+
+findJobs(URL)
+
+nextPage = input("Would you like to view another page? (Yes/No)\n")
+
+pageNumber = 10
+
+if nextPage == "Yes":
+    URL += "&start=" + str(pageNumber)
+    pageNumber + 10
+
+findJobs(URL)
